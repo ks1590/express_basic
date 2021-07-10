18f740a61900abd3cf602618f937dbb03c01d9a1
kensuke suzuki <35k.0101@gmail.com>
2021-07-10 11:13:09 +0900

    logger作成

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "Suzuki",
+    email: "test1@gmail.com",
+    status: "active"
+  },
+  {
+    id: 2,
+    name: "Satou",
+    email: "test2@gmail.com",
+    status: "inactive"
+  },
+  {
+    id: 3,
+    name: "Tanaka",
+    email: "test3@gmail.com",
+    status: "active"
+  }
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const logger = require("./middleware/logger");
+const members = require('./Members');
 
 const app = express();
+
+// app.use(logger);
+
+// module.exports = members;
+// Get All Members
+app.get('/api/members', (req, res) => res.json(members));
 
 // set static folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,4 +21,4 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
